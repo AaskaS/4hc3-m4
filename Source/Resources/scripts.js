f6e9a95114690adc339734789dbd747d2b43f3c6
Aaska Shah <aaska18@gmail.com>
2018-12-02 17:53:23 -0500

    added contact profile page, headings change depending on which name is clicked

--- Source/Resources/scripts.js
+++ Source/Resources/scripts.js
@@ -10,3 +10,84 @@ Nolan Slade 		001401079
 Vyome Kishore 		001406201
 
 */
+
+function getName(){
+	var name = document.getElementById("name1").innerText;
+	var uname = document.getElementById("uname1").innerText;
+	console.log(name);
+	window.localStorage.setItem("name", name);
+	window.localStorage.setItem("uname", uname);
+	window.location='contactProfile.html';
+
+}
+
+function getName2(){
+	var name = document.getElementById("name2").innerText;
+	var uname = document.getElementById("uname2").innerText;
+	console.log(name);
+	window.localStorage.setItem("name", name);
+	window.localStorage.setItem("uname", uname);
+	window.location='contactProfile.html';
+
+}
+
+function getName3(){
+	var name = document.getElementById("name3").innerText;
+	var uname = document.getElementById("uname3").innerText;
+	console.log(name);
+	window.localStorage.setItem("name", name);
+	window.localStorage.setItem("uname", uname);
+	window.location='contactProfile.html';
+
+}
+
+function getName4(){
+	var name = document.getElementById("name4").innerText;
+	var uname = document.getElementById("uname4").innerText;
+	console.log(name);
+	window.localStorage.setItem("name", name);
+	window.localStorage.setItem("uname", uname);
+	window.location='contactProfile.html';
+
+}
+
+function getName5(){
+	var name = document.getElementById("name5").innerText;
+	var uname = document.getElementById("uname5").innerText;
+	console.log(name);
+	window.localStorage.setItem("name", name);
+	window.localStorage.setItem("uname", uname);
+	window.location='contactProfile.html';
+
+}
+
+
+function getName6(){
+	var name = document.getElementById("name6").innerText;
+	var uname = document.getElementById("uname6").innerText;
+	console.log(name);
+	window.localStorage.setItem("name", name);
+	window.localStorage.setItem("uname", uname);
+	window.location='contactProfile.html';
+
+}
+
+function prName(){
+	console.log(window.localStorage.getItem("contact"));
+	document.getElementById("user").innerText = "Profile of " + window.localStorage.getItem("name");
+	document.getElementById("name").innerText = window.localStorage.getItem("name");
+	document.getElementById("uname").innerText = window.localStorage.getItem("uname");
+
+
+
+
+}
+
+function back(){
+	window.localStorage.clear();
+	window.location = 'contacts.html';
+}
+
+
+
+
